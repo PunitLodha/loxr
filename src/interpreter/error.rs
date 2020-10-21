bd21e145656ee00e3d8125976bd8d97d75d87fae
PunitLodha <punitlodha@pm.me>
2020-10-21 12:47:05 +0530

    created CodeError

---
+++ src/interpreter/error.rs
@@ -0,0 +1,17 @@
+use std::fmt;
+#[derive(Debug)]
+pub struct CodeError {
+    line: u32,
+    place: String,
+    message: String,
+}
+
+impl fmt::Display for CodeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "[line {}] Error {}: {}",
+            self.line, self.place, self.message
+        )
+    }
+}
